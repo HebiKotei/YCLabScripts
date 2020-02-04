7abf2b084d7502b72f435a795cb93cdffd57cada
Hebirura <c1yvan@outlook.com>
2020-02-04 06:29:53 +0000

    Update funcunc.py

--- funcunc.py
+++ funcunc.py
@@ -13,8 +13,36 @@ You may modify this script in any fashion you like.
 
 #calling packages & initialising
 import sympy as sym
+import numpy as np
 
 sym.init_printing()
 
-#definitions, pLease add additional symbols to fit your equations. Please ensure to add them onto the left hand side of the equals sign, seperate them with commas. Then in the string, add the symbol again.
-a, b, c, x, y = symbols('a b c x y')
+#definitions, pLease add additional symbols to fit your equations.
+#Please ensure to add them onto the left hand side of the equals sign, seperate them with commas.
+#Then in the 'string', add the symbol again.
+a, b, c, x, y, z = sym.symbols('a b c x y z')
+'''
+any constants in the equation can be definined below as shown:
+    g = 9.81
+    e = 1.602*10**(-19)
+'''
+
+#Define your equation with two variables. This example will be here
+y = 2*x*z
+dx = sym.diff(y, x).doit() #1
+dz = sym.diff(y, z).doit() #2
+
+print('your partial derivatives are:')
+print('dy/dx =', dx)
+print ('dy/dz =', dz)
+
+#using the formula for the compounding of uncertainties for a function with two unknown values.
+#input values in the console for your absolute uncertainties, it'll ask for dx first in the example, then dz's uncertainty
+
+print('Please input the values for the absolute uncertainty of the first variable you are differentiating respect to')
+ux = float(input())
+print('Please input the values for the absolute uncertainty of the second variable you are differentiating respect to')
+uz = float(input())
+
+eq = ((dz**2)*(uz**2)) + ((dx**2)*(ux**2))
+
